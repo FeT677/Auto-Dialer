64ecab9269874dc142cd8a6ee6da5058d39965dd
FetroX <fetroxxxx@gmail.com>
2024-10-21 23:49:32 +0300

    Незначительные изменения

--- find_and_call.py
+++ find_and_call.py
@@ -5,56 +5,43 @@ import webbrowser
 from PIL import ImageGrab
 
 
-#Задание пути к Тесеракту 
-pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+# Задание пути к Тесеракту
+pytesseract.pytesseract.tesseract_cmd = r"Tesseract-OCR\tesseract.exe"
 
 
-#Функция получения текста с картинки в буфере обмена
+# Функция получения текста с картинки в буфере обмена
 def extract_text_from_clipboard_image():
     img = ImageGrab.grabclipboard()
-
     if isinstance(img, list):
         img = img[0]
-
     if isinstance(img, ImageGrab.Image.Image):
         extracted_text = pytesseract.image_to_string(img)
-
         if extracted_text.strip():
-            print(f"Распознанный текст: {extracted_text}")
             pyperclip.copy(extracted_text)
-            print("Текст скопирован в буфер обмена.")
             if find_phone_number(extracted_text):
                 open_softphone_and_call(extracted_text)
         else:
-            print("Текст не найден на изображении.\n\n")
-            pyperclip.copy('')
+            pyperclip.copy("")
             return None
     else:
-        print("Изображение не найдено в буфере обмена.")
-        pyperclip.copy('')
+        pyperclip.copy("")
         return None
-    
 
-#Функция поиска номера на картинке
+
+# Функция отбора номера телефона в распознанном тексте
 def find_phone_number(text):
     try:
-        phone_number = re.findall(r'\+?\d[\d\s-]{7,}\d', text)
+        phone_number = re.findall(r"\+?\d[\d\s-]{7,}\d", text)
     except:
-        print('Не найден номер телефона на изображении\n\n')
-
+        pass
     if phone_number:
-        formatted_phone_number = re.sub(r'[-\s]', '', phone_number[0])
-        print(f"Найден номер телефона: {formatted_phone_number}")
-        
+        formatted_phone_number = re.sub(r"[-\s]", "", phone_number[0])
         return formatted_phone_number
     else:
-        print("Номер телефона не найден.")
-        
         return None
 
 
-#Функция отправки номера телефона в звонилку по протоколу TEL:
+# Функция отправки номера телефона в звонилку по протоколу TEL:
 def open_softphone_and_call(phone_number):
     tel_url = f"tel:{phone_number}"
-    # Открытие номера через протокол tel
     webbrowser.open(tel_url)
--- main.py
+++ main.py
@@ -1,76 +1,71 @@
 import customtkinter as ctk
-from PIL import Image, ImageTk, ImageGrab
+from PIL import ImageGrab
 import threading
 from find_and_call import extract_text_from_clipboard_image
 import time
 
-# Флаг и событие для работы скрипта
+
 script_running = False
 stop_event = threading.Event()
 
-# Функция, которая запускает основной процесс в отдельном потоке
+
+# Запуск
 def run():
     global script_running
-    print('Скрипт запущен\n\n')
     previous_image = None
-
-    previous_image = None
-    previous_copy_time = 0  # Время последнего копирования изображения
+    previous_copy_time = 0
 
     while script_running:
-        # Проверяем буфер обмена на наличие изображения
         img = ImageGrab.grabclipboard()
         if isinstance(img, list):
             img = img[0]
-
         if img:
-            current_time = time.time()  # Текущее время
-            if img != previous_image or (img == previous_image and current_time - previous_copy_time > 1):
+            current_time = time.time()
+            if img != previous_image or (
+                img == previous_image and current_time - previous_copy_time > 1
+            ):
                 extract_text_from_clipboard_image()
                 previous_image = img
-                previous_copy_time = current_time  # Обновляем время последнего копирования
-
-        # Используем событие для уменьшения нагрузки на процессор
+                previous_copy_time = current_time
         stop_event.wait(1)
+
 
 # Функция для включения/выключения скрипта
 def toggle_script():
     global script_running
     if switch_var.get() == 1:
-        if not script_running:  # чтобы не запускать несколько потоков
+        if not script_running:
             script_running = True
-            stop_event.clear()  # Сбрасываем событие, чтобы запустить поток
-            print("Автозвонок включен")
-            toggle_switch.configure(text="Автозвонок включен")
-            # Запускаем run в отдельном потоке
+            stop_event.clear()
+            toggle_switch.configure(text="Распознование ВКЛЮЧЕНО")
             threading.Thread(target=run, daemon=True).start()
     else:
         script_running = False
-        stop_event.set()  # Устанавливаем событие, чтобы остановить поток
-        print("Автозвонок выключен")
-        toggle_switch.configure(text="Автозвонок выключен")
+        stop_event.set()
+        toggle_switch.configure(text="Распознование ВЫКЛЮЧЕНО")
 
-# =================================================== #
-#               Создаем интерфейс                     #
-# =================================================== #
 
-window = ctk.CTk(fg_color='#343535')
-window.title('Auto Dialer')
+##### Создаем интерфейс #####
 
-# Добавляем иконку для окна
+
+window = ctk.CTk(fg_color="#343535")
+window.title("Auto Dialer")
+
 window.iconbitmap(r"Auto Dialer.ico")
 
-window.geometry("250x100")  
+window.geometry("275x100")
 window.resizable(False, False)
 
-border_frame = ctk.CTkFrame(window, fg_color='#565656')
+border_frame = ctk.CTkFrame(window, fg_color="#565656")
 border_frame.pack(padx=5, pady=5, fill="both", expand=True)
 
-CTkFrame = ctk.CTkFrame(border_frame, fg_color='#3D3D3D')
+CTkFrame = ctk.CTkFrame(border_frame, fg_color="#3D3D3D")
 CTkFrame.pack(padx=10, pady=10, fill="both", expand=True)
 
-switch_var = ctk.IntVar()  
-toggle_switch = ctk.CTkSwitch(CTkFrame, text="Автозвонок выключен", variable=switch_var, command=toggle_script)
+switch_var = ctk.IntVar()
+toggle_switch = ctk.CTkSwitch(
+    CTkFrame, text="Распознование ВЫКЛЮЧЕНО", variable=switch_var, command=toggle_script
+)
 toggle_switch.place(relx=0.5, rely=0.5, anchor="center")
 
 window.mainloop()
